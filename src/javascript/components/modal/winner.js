a105c54b162f879d2c4637a8d447d470f36e4ce0
velisssa <tybinka.vasylyna@gmail.com>
2023-12-14 21:12:44 +0100

    + change winner modal

--- src/javascript/components/modal/winner.js
+++ src/javascript/components/modal/winner.js
@@ -1,5 +1,4 @@
 import showModal from './modal';
-import createElement from '../../helpers/domHelper';
 import { createFighterImage } from '../fighterPreview';
 
 export default function showWinnerModal(fighter) {
@@ -9,11 +8,7 @@ export default function showWinnerModal(fighter) {
         window.location.reload();
     };
 
-    const bodyElement = createElement({ tagName: 'div', className: 'modal-body' });
-    const imageElement = createFighterImage(fighter);
-    const infoElement = createElement({ tagName: 'h3', className: 'modal-info' });
+    const bodyElement = createFighterImage(fighter);
 
-    infoElement.innerText = `${fighter.name} is a winner!`;
-    bodyElement.append(imageElement, infoElement);
-    showModal({ title: "Fight's result:", bodyElement, onClose });
+    showModal({ title: `${fighter.name} is a winner!`, bodyElement, onClose });
 }
